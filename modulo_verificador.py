cc9392ca0c22740d0646a7b4250056d75c91eeaa
Juan Jose Varela <jjvalela0203@gmail.com>
2026-03-19 08:45:55 -0500

    Delete modulo_verificador.py

--- modulo_verificador.py
+++
@@ -1,9 +0,0 @@
-def verificar_ganador (h_hp, e_hp):
-    if h_hp <= 0:
-        print("Tu vida ha llegado a 0, has sido derrotado")
-        return True
-    if e_hp <= 0:
-        print("Has derrotado a tu enemigo")
-        return True 
-    else:
-        return False
