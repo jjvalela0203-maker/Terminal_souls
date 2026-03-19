e4ca981fe343df7c4fac63ace43311fcdfd38433
Juan Jose Varela <jjvalela0203@gmail.com>
2026-03-19 08:46:16 -0500

    Delete modulo_estado.py

--- modulo_estado.py
+++
@@ -1,6 +0,0 @@
-def estado (nombre_h, h_hp, nombre_e, e_hp):
-    print(f"{nombre_h} tiene {h_hp} puntos de vida")
-    print(f"{nombre_h} ha recibido {h_hp} puntos de daño")  
-    print(f"{nombre_e} tiene {e_hp} puntos de vida ")
-    print(f"{nombre_e} ha recibido {h_hp} puntos de daño")
-    
